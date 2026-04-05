12716cd4005a0a42539e7bc691d2e9821b88d9d7
Kazure Zheng <kazure@debian>
2026-04-05 18:50:47 +0800

    finish logicalNeg

--- datalab/datalab-handout/bits.c
+++ datalab/datalab-handout/bits.c
@@ -237,7 +237,7 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4
  */
 int logicalNeg(int x) {
-  return 2;
+    return ((x | (~x + 1)) >> 31) + 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
